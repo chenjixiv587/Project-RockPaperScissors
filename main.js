4dc6d4ecc583577052911c84e640260190c32f8e
chenjixiv587 <lb5077735@gmail.com>
2024-05-17 19:42:57 +0800

    Write the logic to play a single round

--- main.js
+++ main.js
@@ -6,9 +6,48 @@ function getComputerChoice(choices) {
     return choices[choiceIndex];
 }
 
-console.log(getComputerChoice(choices));
+// console.log(getComputerChoice(choices));
 function getHumanChoice() {
     const humanChoice = prompt("What do you choose: 'rock, paper, scissors'? ");
-    return humanChoice;
+    return humanChoice.toLowerCase();
 }
 // console.log(getHumanChoice());
+function choiceToValue(choice) {
+    let value = 0;
+    if (choice === "scissors") {
+        value = 1;
+    } else if (choice === "rock") {
+        value = 2;
+    } else if (choice === "paper") {
+        value = 3;
+    }
+    return value;
+}
+function winOrLose(value1, value2) {
+    const result = value1 - value2;
+    if (result === 0) {
+        return "Equal";
+    }
+    else if (result === -2 || result === 1) {
+        return "Computer win";
+    }
+    else if (result === -1 || result === 2) {
+        return "You win";
+    }
+}
+function playRound(humanSelection, computerSelection) {
+    let res = winOrLose(choiceToValue(computerSelection), choiceToValue(humanSelection));
+    if (res === "Computer win") {
+        return `${res}! ${computerSelection} beats ${humanSelection}`
+    } else if (res === "You win") {
+        return `${res}! ${humanSelection} beats ${computerSelection}`
+    } else {
+        return res;
+    }
+
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(choices);
+
+const playedResult = playRound(humanSelection, computerSelection);
+console.log(playedResult);
